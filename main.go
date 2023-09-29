da5e5c90e4759dc0e79a751371dc74b0223007e7
Shawn Kost <shawnmkost@gmail.com>
2023-09-29 15:23:34 -0700

    Add functionality to read deck from hard drive

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 func main() {
-	cards := newDeck()
-	cards.saveToFile("my_cards")
-
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
