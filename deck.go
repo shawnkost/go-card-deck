3b35bd7d89f04aca5836a462f860ab534d72ebdc
Shawn Kost <shawnmkost@gmail.com>
2023-09-29 13:54:25 -0700

    Add deal functionality

--- deck.go
+++ deck.go
@@ -12,15 +12,21 @@ func newDeck() deck {
 	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	for i, suit := range cardSuits {
-		for j, value := range cardValues {
-			cards = append(cards, suit+" of "+value)
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
 		}
 	}
+
+	return cards
 }
 
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
