efca640879e44ca1f7cb20f066fd51fef6010e51
Shawn Kost <shawnmkost@gmail.com>
2023-09-29 11:30:00 -0700

    WIP: Add new deck functionality

--- deck.go
+++ deck.go
@@ -2,9 +2,22 @@ package main
 
 import "fmt"
 
-// Create a new type of 'deck
+// Create a new type of 'deck'
 // which is a slice of strings
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for i, suit := range cardSuits {
+		for j, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+}
 
 func (d deck) print() {
 	for i, card := range d {
