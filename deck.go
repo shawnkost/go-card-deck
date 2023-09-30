1ce2ef6387fae9f056e7f85c2119a8ef47f73edf
Shawn Kost <shawnmkost@gmail.com>
2023-09-30 08:47:00 -0700

    Utilize Sprintf for concatenation and implement true randomness

--- deck.go
+++ deck.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -19,7 +20,7 @@ func newDeck() deck {
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, value+" of "+suit)
+			cards = append(cards, fmt.Sprintf("%s of %s", value, suit))
 		}
 	}
 
@@ -56,8 +57,11 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
